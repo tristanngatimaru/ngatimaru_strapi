fa54419b1caae7530a5dc6edb98d00524c054d7b
tristanngatimaru <tristan@ngatimaru.iwi.nz>
2026-01-19 11:36:09 +1300

    plugin changes

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,4 +2,21 @@ module.exports = {
   documentation: {
     enabled: true,
   },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.SMTP_USERNAME,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      settings: {
+        defaultFrom: process.env.SMTP_DEFAULT_FROM,
+        defaultReplyTo: process.env.SMTP_DEFAULT_REPLY_TO,
+      },
+    },
+  },
 };
